901c8bfa7404c3b21b9ae55705918725a60e60aa
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2026-08-19 13:44:02 +0200

    Adapts GIPSL spec to only contain S1, S2, S5, S6, S7, S8

--- ihtcvirtualrunner/src/org/emoflon/gips/ihtc/virtual/runner/IhtcVirtualGipsRunner.java
+++ ihtcvirtualrunner/src/org/emoflon/gips/ihtc/virtual/runner/IhtcVirtualGipsRunner.java
@@ -219,9 +219,6 @@ public class IhtcVirtualGipsRunner extends AbstractIhtcVirtualGipsRunner {
 			totalVars += logVarStats(gipsApi.getSurgeonInOT());
 			totalVars += logVarStats(gipsApi.getSurgeonPenalizedOTs());
 			totalVars += logVarStats(gipsApi.getAgeGroupsInRoom());
-			totalVars += logVarStats(gipsApi.getAssignedNursesToWorkload());
-			totalVars += logVarStats(gipsApi.getNurseWorkloadForDay());
-			totalVars += logVarStats(gipsApi.getAssignedNurseForPatient());
 
 			logger.info("Total estimated number of variables: " + totalVars);
 		}
